8bc21f28801cc7a83d0b27cfe9e2e7a5cb34b4b4
WShqwerty <3168923454@qq.com>
2023-09-22 17:26:52 +0800

    Update README.md

--- 网络分析/test1.py
+++ 网络分析/test1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import igraph as ig
 import csv
 
@@ -14,15 +16,46 @@ def read_from_csv(filename):
     return result
 
 
+def dict_to_list(data):
+    datas = []
+    for key, value in data.items():
+        datas.append([key, value])
+    return datas
+
+
+def write_to_csv(data, file):
+    with open(file, "w", newline="") as f:
+        writer = csv.writer(f)
+        # writer.writerow(header)
+        count = 1
+        for da in data:
+            writer.writerow(da)
+            percent = int(count / len(data) * 50)
+            print(f'\r[{"#"*percent}{"."*(50-percent)}]\t{count}/{len(data)}', end=".")
+            count += 1
+        print()
+        f.close()
+
+
 if __name__ == "__main__":
-    # datas = read_from_csv("边.csv")
-    datas = read_from_csv("node.csv")
+    datas = read_from_csv("边.csv")
+    # datas = read_from_csv("node.csv")
     datas_for_graph = []
     datas_total = []
     for data in datas:
         datas_for_graph.append((data[0], data[1]))
         datas_total.append(data[0])
         datas_total.append(data[1])
+
+    datas_count = Counter(datas_total)
+    datas_count = dict_to_list(datas_count)
+    datas_temp = []
+    for data in datas_count:
+        if data[1] >= 9:
+            datas_temp.append(data)
+    datas_temp.insert(0, ["节点名", "连接的度数"])
+    write_to_csv(datas_temp, "result.csv")
+
     datas_total = list(set(datas_total))
     g = ig.Graph()
     g.add_vertices(datas_total)
@@ -31,7 +64,7 @@ if __name__ == "__main__":
 
     layout = g.layout_fruchterman_reingold()
     ig.plot(g, layout=layout, target="test1.png")
-    
+
     print("度", end=':')
     print(g.degree())
     ebs = g.edge_betweenness()
